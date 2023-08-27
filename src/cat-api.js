910297ec837885dce03e2d5f8ac6e323da358c20
OlegShinka <volkonxxl@gmail.com>
2023-08-27 20:30:34 +0300

    axios

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,22 +1,21 @@
-function fetchBreeds() {
-  return fetch(
-    'https://api.thecatapi.com/v1/breeds?api_key=live_fT5OA78enT6cSqE2C76XRxJrxP0Ra57b2VF7j9UBjbhJ8k3SoE5hDitOkAhrLMiP'
-  ).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  });
-}
+import axios from 'axios';
+
+axios.defaults.headers.common['x-api-key'] =
+  'live_fT5OA78enT6cSqE2C76XRxJrxP0Ra57b2VF7j9UBjbhJ8k3SoE5hDitOkAhrLMiP';
+
 function fetchCatByBreed(breedId) {
-  return fetch(
-    `https://api.thecatapi.com/v1/images/search?api_key=live_fT5OA78enT6cSqE2C76XRxJrxP0Ra57b2VF7j9UBjbhJ8k3SoE5hDitOkAhrLMiP&breed_ids=${breedId}`
-  ).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    console.log(response);
-    return response.json();
+  return axios
+    .get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}&`)
+    .then(response => {
+      console.log('response_fetchByBreed', response);
+
+      return response;
+    });
+}
+
+function fetchBreeds() {
+  return axios.get('https://api.thecatapi.com/v1/breeds').then(res => {
+    return res;
   });
 }
 
--- src/index.js
+++ src/index.js
@@ -14,19 +14,36 @@ refs.loaderCircle.classList.add('js-hidden');
 refs.errorEl.classList.add('js-hidden');
 refs.loaderEl.classList.add('js-hidden');
 
+const defaults = {
+  nameDef: 'Name not found',
+  idDEF: 'Id not found',
+  urlDef:
+    'https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/19452/cartoon-cat-face-clipart-md.png',
+  altDef: 'Title not found',
+  descDef: 'Information not found',
+};
+
 refs.selectEl.addEventListener('change', onBreedSelect);
 function onBreedSelect(evt) {
   refs.infoEl.innerHTML = '';
   refs.loaderCircle.classList.remove('js-hidden');
-
-  fetchCatByBreed(evt.currentTarget.value)
+  const selectValue = evt.currentTarget.value;
+  fetchCatByBreed(selectValue)
     .then(catBreed => {
-      const cat = catBreed[0];
-      const markup = `<div><img src="${cat.url}" alt="${cat.url}" width = 500/></div>
+      console.log('catBreed.data[0]', catBreed.data[0]);
+      const cat = catBreed.data[0];
+      console.log('cat.url', cat.url);
+      console.log('cat.breeds', cat);
+      const markup = `<img src="${cat.url || defaults.urlDef}" alt="${
+        cat.url || defaults.altDef
+      }" width = 500/>
       <div class="cat-disc">
-       <h2>${cat.breeds[0].name}</h2>
-       <p>${cat.breeds[0].description}</p>
-       <p><b>Temperament: </b>${cat.breeds[0].temperament}</p> </div>`;
+
+       <h2>${cat.breeds[0].name || defaults.nameDef}</h2>
+       <p>${cat.breeds[0].description || defaults.descDef}</p>
+       <p><b>Temperament: </b>${
+         cat.breeds[0].temperament || defaults.descDef
+       }</p> </div>`;
 
       refs.infoEl.innerHTML = markup;
     })
@@ -43,15 +60,16 @@ function onBreedSelect(evt) {
 function markupSelect(arr) {
   const markupSel = arr
     .map(({ id, name }) => {
-      return ` <option value="${id}">${name}</option>`;
+      return ` <option value="${id || defaults.idDef}">${
+        name || defaults.nameDef
+      }</option>`;
     })
     .join('');
   refs.selectEl.insertAdjacentHTML('beforeend', markupSel);
 }
-
 fetchBreeds()
-  .then(markupSel => {
-    markupSelect(markupSel);
+  .then(res => {
+    markupSelect(res.data);
   })
   .catch(err => {
     Notiflix.Report.warning(
